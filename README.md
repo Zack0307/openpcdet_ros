26b5a19cc90fb5a0dac1c86498db3c10213571d9
Zack930307 <box930307@gmail.com>
2025-07-30 08:58:04 +0800

    upload again

---
+++ README.md
@@ -0,0 +1,84 @@
+OpenPCDet ROS2 Running and Visualization
+---
+
+Effect pictures:
+
+
+Test computer and System:
+
+- Desktop setting: i5-10400, GPU 3080, CUDA 11.3
+- System setting: Ubuntu 22.04, ROS2 Humble (Python 3.10)
+- Test Date: 2025/07/25
+
+If you don't want to destroy your env/change your system version, please directly <a href="#Docker">check the docker and build dockerfile</a>. But you still have to have a computer with NVIDIA-GPU and can install cuda. Check [Chinese blog Ubuntu下的NVIDIA显卡【驱动&CUDA 安装与卸载】](https://www.cnblogs.com/kin-zhang/p/17007246.html) 
+
+Dataset: [2011_09_26_drive_0005](https://www.cvlibs.net/datasets/kitti/raw_data.php), ([synced+rectified data] [calibration]) 
+tracking Label:[2011_09_26_drive_0005](https://s3.eu-central-1.amazonaws.com/avg-kitti/data_tracking_label_2.zip) [0000.txt]
+PS: Login account
+
+## Build & RUN
+
+Dependencies:
+
+ros2_numpy:https://github.com/YoushaaMurhij/ros2_numpy.git
+
+```bash
+sudo apt install ros-humble-vision-msgs
+pip install pyquaternion
+```
+
+And install the [OpenPCDet](https://github.com/open-mmlab/OpenPCDet) in the env
+```bash
+pip install torch==1.10.0+cu113 torchvision==0.11.0+cu113 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install spconv-cu113
+sudo apt-get install python-setuptools
+
+pip install pyquaternion install numpy==1.23 pillow==8.4
+# have some potential risks
+sudo chown -R $USER /usr/local/lib/python3.10/
+sudo chown -R $USER /usr/local/bin/
+# have some potential risks
+
+git clone https://github.com/Zack0307/openpcdet_ros.git
+cd OpenPCDet && python3 setup.py develop
+```
+
+Run:
+```bash
+mkdir -p ~/workspace/OpenPCDet_ws/src
+cd ~/workspace/OpenPCDet_ws/src
+git clone https://github.com/zack0307/OpenPCDet_ros.git
+cd .. && colcon build
+```
+
+One more step check your [Config file](launch/config.yaml)
+
+```bash
+source /opt/ros/humble/setup.bash
+# before this step please change the model path in config file
+ros2 launch openpcdet 3d_object_detector.launch
+```
+
+## Issue/TODO
+
+- [ ] finish README.md`
+
+- [ ] CaDDN模組 需要深度圖
+
+
+
+## Other infos
+
+This repo has a Chinese blog also to read through [【点云检测】OpenPCDet 教程系列 [1] 安装 与 ROS运行](https://www.cnblogs.com/kin-zhang/p/17002980.html)
+
+### Acknowledgement
+
+All methods and models are from: [open-mmlab/OpenPCDet](https://github.com/open-mmlab/OpenPCDet)
+
+Reference codes:
+
+1. The first version of openpcdet-ros is from: [Cram3r95/OpenPCDet-ROS](https://github.com/Cram3r95/OpenPCDet-ROS)
+
+2. For 3d box marker drawing: [Youtube AI葵](https://www.youtube.com/watch?v=nIiqo3ZuFCc&list=PLDV2CyUo4q-L4YlXUWDytZPz9a8cAWXST&index=11&ab_channel=AI%E8%91%B5) and [his code](https://github.com/kwea123/ROS_notes)
+
+
---
+++ openpcdet_ros/pcdet_utils.py
@@ -0,0 +1,137 @@
+import rclpy
+import cv2 as cv
+import math
+import time
+import glob
+import torch
+import os
+import pandas as pd
+import ros2_numpy
+import numpy as np
+from sensor_msgs.msg import Image, PointCloud2, PointField
+from std_msgs.msg import Header
+from sensor_msgs_py import point_cloud2 as pcl2
+from visualization_msgs.msg import Marker, MarkerArray
+from rclpy.node import Node
+from cv_bridge import CvBridge
+from PIL import Image as PILImage
+from pathlib import Path
+from openpcdet_ros.draw3d_utils import *
+from openpcdet_ros.__init__ import *
+import yaml
+import sys
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+cv_bridge = CvBridge()
+Image_DATA_PATH = '/home/zack/kitti/2011_09_26_drive_0005_sync/2011_09_26/2011_09_26_drive_0005_sync'
+data_number = len(os.listdir(os.path.join(Image_DATA_PATH, 'image_02/data')))
+sys.path.append("/home/zack/Desktop/OpenPCDet")
+
+# OpenPCDet imports
+from pcdet.datasets import DatasetTemplate
+from pcdet.models import build_network, load_data_to_gpu
+from pcdet.config import cfg, cfg_from_yaml_file
+from pcdet.utils import box_utils, calibration_kitti, common_utils, object3d_kitti
+FRAME_ID = 'map'  # 依你的座標框架而定
+TRACKING_COLUMN_NAMES = ['frame', 'track id', 'type', 'truncated', 'occluded', 'alpha', 'bbox_left', 'bbox_top', 'bbox_right', 'bbox_bottom', 
+              'height', 'width', 'length', 'loc_x', 'loc_y', 'loc_z', 'rot_y' ]
+
+#data
+df = pd.read_csv('/home/zack/kitti/data_tracking_label_2/training/label_02/0000.txt', header=None, sep=' ')
+df.columns = TRACKING_COLUMN_NAMES 
+df.loc[df.type.isin(['Van','Truck','Tram']), 'type'] = 'Car'
+df = df[df.type.isin(['Car','Pedestrian','Cyclist'])]
+
+
+#config file
+
+BASE_DIR = "/home/zack/ros2_ws/src/openpcdet_ros"
+with open(f"{BASE_DIR}/launch/config.yaml", 'r') as f:
+    try:
+        para_cfg = yaml.safe_load(f, Loader=yaml.FullLoader)
+    except:
+        para_cfg = yaml.safe_load(f)
+
+cfg_root = para_cfg["cfg_root"]
+model_path = para_cfg["model_path"]
+threshold = para_cfg["threshold"]
+pointcloud_topic = para_cfg["pointcloud_topic"]
+RATE_VIZ = para_cfg["viz_rate"]
+inference_time_list = []
+
+# config = cfg_from_yaml_file(cfg.ROOT_DIR / 'tools/cfgs/dataset_configs/kitti_dataset.yaml', cfg)
+
+
+def cam_publish(frame, cam_pub):
+    cap = cv.VideoCapture(0)
+    ret, frame = cap.read()
+          
+    if ret == True:
+        imgmsg = cv_inference(frame)
+        cam_pub.publish(cv_bridge.cv2_to_imgmsg(frame, encoding = 'bgr8'))
+
+def image_publish(frame, cam_pub):
+    img = cv.imread(os.path.join(Image_DATA_PATH, 'image_02/data/%010d.png'%frame))
+
+    boxes = np.array(df[df.frame==frame][['bbox_left', 'bbox_top', 'bbox_right', 'bbox_bottom']])
+    types = np.array(df[df.frame==frame]['type'])
+ 
+    for typ, box in zip(types, boxes):
+        top_left = int(box[0]), int(box[1])
+        bottom_right = int(box[2]), int(box[3])
+        cv.rectangle(img, top_left, bottom_right, DETECTION_COLOR_MAP[typ], 2)
+        cv.putText(img, typ, (int(box[0]), int(box[1]) - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    cam_pub.publish(cv_bridge.cv2_to_imgmsg(img, encoding='bgr8'))
+
+def publish_pcl(frame, PCL, clock):
+    pcl = np.fromfile(os.path.join(Image_DATA_PATH, 'velodyne_points/data/%010d.bin' % frame), dtype=np.float32).reshape(-1, 4)
+    header = Header()
+    header.stamp = clock.now().to_msg()
+    header.frame_id = FRAME_ID
+    PCL.publish(pcl2.create_cloud_xyz32(header, pcl[:, :3]))
+
+    
+def cv_inference(frame):
+        # img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        # img = PILImage.fromarray(img)
+        # img = transforms(img).unsqueeze(0)
+        # if torch.cuda.is_available():
+        #     img = img.to(device)
+        # with torch.no_grad():
+        #     output = model(img)['out'][0]   #['out']：取出主要輸出張量
+        # output_predictions = output.argmax(0)
+        # output_predictions = output_predictions.byte().cpu().numpy()
+        # output_predictions = cv.applyColorMap(output_predictions, cv.COLORMAP_JET)
+        # output_predictions = cv.cvtColor(output_predictions, cv.COLOR_RGB2BGR)
+        # img_tomsg = cv_bridge.cv2_to_imgmsg(output_predictions, encoding='bgr8')
+        # return img_tomsg
+        pass
+
+
+def xyz_array_to_pointcloud2(points_sum, stamp=None, frame_id=None):
+    """
+    Create a sensor_msgs.PointCloud2 from an array of points.
+    """
+    msg = PointCloud2()
+    if stamp:
+        msg.header.stamp = stamp
+    if frame_id:
+        msg.header.frame_id = frame_id
+    msg.height = 1
+    msg.width = points_sum.shape[0]
+    msg.fields = [
+        PointField('x', 0, PointField.FLOAT32, 1),
+        PointField('y', 4, PointField.FLOAT32, 1),
+        PointField('z', 8, PointField.FLOAT32, 1)
+        # PointField('i', 12, PointField.FLOAT32, 1)
+        ]
+    msg.is_bigendian = False
+    msg.point_step = 12
+    msg.row_step = points_sum.shape[0]
+    msg.is_dense = int(np.isfinite(points_sum).all())
+    msg.data = np.asarray(points_sum, np.float64).tostring()
+    return msg
+
+# if __name__ == "__main__":
+#     print(cfg_root)
+#     print(config)
